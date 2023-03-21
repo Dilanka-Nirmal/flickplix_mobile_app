b123dbe37df485702310fea4f6effe3568876e63
Dilanka Nirmal <dilankanirmal138@gmail.com>
2023-03-22 01:44:28 +0530

    Reminder list connected with the Firestore

---
+++ src/Components/Reminder/AddReminderModal.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import COLORS from '../../Components/Animation/Colors';
+
+export default AddReminderModal = ({handleTodoVisible, addReminderList}) => {
+  // Define Colors
+  const backgroundColors = [
+    '#5CD859',
+    '#24A6D9',
+    '#595BD9',
+    '#8022D9',
+    '#D159D8',
+    '#D85963',
+    '#D88559',
+  ];
+
+  const [getSelectedColor, setSelectedColor] = React.useState(
+    backgroundColors[0],
+  );
+
+  const [getName, setName] = React.useState('');
+
+  // Create ToDo
+  const createToDo = () => {
+    const [name, color] = [getName, getSelectedColor];
+
+    const list = {name, color};
+    addReminderList(list);
+    setName('');
+    handleTodoVisible();
+  };
+
+  // Render Colors
+  const renderColors = () => {
+    return backgroundColors.map(color => {
+      return (
+        <TouchableOpacity
+          key={color}
+          style={[styles.colorSelect, {backgroundColor: color}]}
+          onPress={() => setSelectedColor(color)}
+        />
+      );
+    });
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <TouchableOpacity
+        style={{position: 'absolute', top: 64, right: 32}}
+        onPress={handleTodoVisible}>
+        <Icon name="close" size={24} color={COLORS.black} />
+      </TouchableOpacity>
+
+      <View style={{alignSelf: 'stretch', marginHorizontal: 32}}>
+        <Text style={styles.title}>Create Todo List</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter The List Name"
+          onChangeText={text => setName(text)}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 12,
+          }}>
+          {renderColors()}
+        </View>
+        <TouchableOpacity
+          style={[styles.create, {backgroundColor: getSelectedColor}]}
+          onPress={createToDo}>
+          <Text style={{color: COLORS.white, fontWeight: '600'}}>Create!</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '800',
+    color: COLORS.black,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: COLORS.blue,
+    borderRadius: 6,
+    height: 50,
+    marginTop: 8,
+    paddingHorizontal: 16,
+    fontSize: 18,
+  },
+  create: {
+    marginTop: 24,
+    height: 50,
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.blue,
+  },
+  colorSelect: {
+    width: 30,
+    height: 30,
+    borderRadius: 4,
+  },
+});
--- src/Features/Reminder.js
+++ src/Features/Reminder.js
@@ -1,10 +1,172 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from 'react-native';
 
-export const Reminder = () => {
+import {onAuthStateChanged} from 'firebase/auth';
+import {
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  onSnapshot,
+  query,
+} from 'firebase/firestore';
+import {auth, db} from '../../firebase';
+
+import COLORS from '../Components/Animation/Colors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ReminderList from '../Components/Reminder/ReminderList';
+import AddReminderModal from '../Components/Reminder/AddReminderModal';
+
+export const Reminder = ({navigation}) => {
+  const [getUser, setUser] = React.useState(null);
+  const [getTodoVisible, setTodoVisible] = React.useState(false);
+  const [getReminderList, setReminderList] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   if (getUser) {
+  //     loadToDoList();
+  //   }
+  // }, [getUser]);
+
+  React.useEffect(() => {
+    if (getUser) {
+      const unsubscribe = loadToDoList();
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [getUser]);
+
+  // Check if user is logged in
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      const uid = user.uid;
+      setUser(uid);
+    } else {
+      navigation.navigate('LoginScreen');
+    }
+  });
+
+  const loadToDoList = () => {
+    const q = query(collection(db, 'users', getUser, 'lists'));
+
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      const lists = [];
+
+      querySnapshot.forEach(doc => {
+        lists.push({id: doc.id, ...doc.data()});
+      });
+
+      setReminderList(lists);
+    });
+
+    return unsubscribe;
+  };
+
+  const handleTodoVisible = () =>
+    getTodoVisible ? setTodoVisible(false) : setTodoVisible(true);
+
+  const renderList = list => {
+    return <ReminderList list={list} updateReminderList={updateReminderList} />;
+  };
+
+  const addReminderList = list => {
+    setReminderList([
+      ...getReminderList,
+      {
+        ...list,
+        id: getReminderList.length + 1,
+        todos: [],
+      },
+    ]);
+  };
+
+  const updateReminderList = list => {
+    setReminderList(
+      getReminderList.map(item => {
+        return item.id === list.id ? list : item;
+      }),
+    );
+  };
+
   return (
-    <View>
-      <Text>Reminder</Text>
+    <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        visible={getTodoVisible}
+        onRequestClose={handleTodoVisible}>
+        <AddReminderModal
+          handleTodoVisible={handleTodoVisible}
+          addReminderList={addReminderList}
+        />
+      </Modal>
+      <View styles={{flexDirection: 'row'}}>
+        {/* <View style={styles.divider} /> */}
+        <Text style={styles.title}>
+          ToDo <Text style={{fontWeight: '300', color: COLORS.blue}}>List</Text>
+        </Text>
+        {/* <View style={styles.divider} /> */}
+      </View>
+      <View style={{marginVertical: 48}}>
+        <TouchableOpacity style={styles.addList} onPress={handleTodoVisible}>
+          <Icon name="movie-open-edit-outline" size={32} color={COLORS.blue} />
+        </TouchableOpacity>
+
+        <Text style={styles.add}>Add List</Text>
+      </View>
+
+      <View style={{height: 275, paddingLeft: 32}}>
+        <FlatList
+          data={getReminderList}
+          keyExtractor={item => item.id.toString()}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item}) => renderList(item)}
+          keyboardShouldPersistTaps="always"
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  divider: {
+    backgroundColor: '#A7CBD9',
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: '800',
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: COLORS.blue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  add: {
+    color: COLORS.blue,
+    fontWeight: '600',
+    fontSize: 14,
+    marginTop: 8,
+  },
+});
